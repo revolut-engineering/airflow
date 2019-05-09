64f704d1f52a29f59542665f5775933169252dc9
Canh Duong <canh.duong@revolut.com>
2019-05-09 11:46:06 +0100

    Fix js deps

--- setup.py
+++ setup.py
@@ -277,7 +277,7 @@ else:
 def do_setup():
     write_version()
     setup(
-        name='apache-airflow',
+        name='revolut-airflow',
         description='Programmatically author, schedule and monitor data pipelines',
         long_description=long_description,
         long_description_content_type='text/markdown',
